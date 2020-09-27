f37fbf6aa158c01ddad967767a3637e18ad3527f
Adam Cowdy <cowdyadam@gmail.com>
2020-09-27 17:41:40 +0100

    Ensure text input holds state

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './styles/TextInput.scss';
 
-export class TextInput extends React.Component<{}, {}> {
+export class TextInput extends React.Component<{}, {value: string}> {
+    constructor(props) {
+        super(props);
+        this.state = {value: ""};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
     render() {
         return (
-            <input type="text"/>
+            <input type="text" value={this.state.value} onChange={this.handleChange}/>
         );
     }
 }
